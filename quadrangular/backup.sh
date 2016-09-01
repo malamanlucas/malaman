f12b48d3dcf35d9603bfda3df435f9f90c34138b
Lucas Malaman <malamanlucas@gmail.com>
2016-09-01 00:10:36 -0300

    a

---
+++ quadrangular/backup.sh
@@ -0,0 +1,1 @@
+/usr/bin/pg_dump --host localhost --port 5432 --username "postgres" --no-password  --format plain --no-owner --encoding UTF8 --inserts --column-inserts --no-privileges --no-tablespaces --no-unlogged-table-data --file "/home/lucas/projetos/malaman/quadrangular/src/main/resources/backup_capitulos" "postgres"
